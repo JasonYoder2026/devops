4097c9caea8b822cf0ff9f36a22cee9bd854f3c0
Jason Yoder <jason.yoder2@bsu.edu>
2025-08-28 14:05:34 +0000

    Adds restrictions for negative numbers

--- test/test.js
+++ test/test.js
@@ -12,3 +12,10 @@ describe('First two terms', function() {
       assert.strictEqual(main.fibonacci(1), 1);
     });
 });
+
+describe("Negative numbers", function() {
+    it("should return undefined", function(){
+        assert.strictEqual(main.fibonacci(-3), undefined)
+        assert.strictEqual(main.fibonacci(-1), undefined)
+    });
+});
